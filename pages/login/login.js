8ba45ff5004ee7dafe25e7fce731d70df2694ecf
giokubla <giorgi.kublashvili2000@gmail.com>
2023-01-16 22:44:06 +0400

    setup authentification

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,27 +1,27 @@
 import { authentificationResponse } from '../../fake-store/main.js'
 
 
+let input1 = document.querySelector('#exampleInputEmail1');
+let form = document.querySelector('#form')
+let input2 = document.querySelector('#exampleInputPassword1');
+let errorDiv = document.querySelector('.invalid-feedback')
 
+form.addEventListener('submit', onSubmit)
 
-let input1 = document.querySelector('.input-1');
-let input2 = document.querySelector('.input-2');
-let button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-    var performance;
-    if (input1 == receivedPromise.userName && input2 == receivedPromise.password) {
-        return performance = true;
-    } else {
-        performance = false
-    };
-    if (performance === true) {
-        window.onload = () => {
-            window.location.href = 'https://www.google.com/search?q=translate&oq=tra&aqs=chrome.1.69i57j35i39j0i67l2j69i60l3j69i65.2698j0j7&sourceid=chrome&ie=UTF-8'
-        };
-    } else {
-        console.log(performance)
-        console.log(receivedPromise)
-        // alert("Sorry, your entered data does not exist")
+function onSubmit($event) {
+    $event.preventDefault();
+    let user = {
+        userName: input1.value,
+        password: input2.value
     }
+ authentificationResponse(user).then(response => {
+   //  console.log('მოსული დატა', response)
+   response.token ? window.location.href = '../main/main.html' : null
+    
+ }).catch(error => {
+    console.log(error)
+    errorDiv.style.display = 'block'
+    errorDiv.innerHTML = error.msg
+ })
+    console.log(input1.value, input2.value)
 }
-)

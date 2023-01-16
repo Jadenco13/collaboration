79a3d362f78cc74da837d29d378c2e0d193380a8
Jadenco13 <giga.begi13@gmail.com>
2023-01-16 20:41:40 +0400

    input problem

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,7 +1,27 @@
-import {authentificationResponse} from '../../fake-store/main.js'
+import { authentificationResponse } from '../../fake-store/main.js'
 
-const receivedPromise = authentificationResponse({
-    userName: 'admin',
-    password: 12345
-})
-console.log(receivedPromise)
+
+
+
+let input1 = document.querySelector('.input-1');
+let input2 = document.querySelector('.input-2');
+let button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    var performance;
+    if (input1 == receivedPromise.userName && input2 == receivedPromise.password) {
+        return performance = true;
+    } else {
+        performance = false
+    };
+    if (performance === true) {
+        window.onload = () => {
+            window.location.href = 'https://www.google.com/search?q=translate&oq=tra&aqs=chrome.1.69i57j35i39j0i67l2j69i60l3j69i65.2698j0j7&sourceid=chrome&ie=UTF-8'
+        };
+    } else {
+        console.log(performance)
+        console.log(receivedPromise)
+        // alert("Sorry, your entered data does not exist")
+    }
+}
+)
